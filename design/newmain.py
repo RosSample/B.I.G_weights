4a3cd6a6f9516bcc7965c7824f279c7304556165
RosSample <ros.3@mail.ru>
2021-10-18 17:09:43 +0300

    watch me [[heaven]]

--- design/newmain.py
+++ design/newmain.py
@@ -1,4 +1,5 @@
 import sys
+import serial
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
@@ -6,19 +7,37 @@ from PyQt5.QtGui import QIcon
 from design import Ui_Weights  # импорт нашего сгенерированного файла
 
 
+def measure():
+    ser.write("m".encode())
+
+
+def port_search():  # поиск портов
+    ports = ['COM%s' % (i + 1) for i in range(256)]
+
+    result = []
+    for port in ports:
+        try:
+            se = serial.Serial(port)
+            se.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+ser = serial.Serial(port_search()[0])  # открытие порта
+
+
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_Weights()
         self.ui.setupUi(self)
-        self.setWindowTitle('Весы')     # название программы
-        self.setWindowIcon(QIcon('./images/icon.png'))      # иконка программы
+        self.setWindowTitle('Весы')  # название программы
+        self.setWindowIcon(QIcon('./images/icon.png'))  # иконка программы
 
         self.show()
-        self.ui.saveButton.clicked.connect(self.buttonClicked)
-
-    def buttonClicked(self):
-        print('button clicked!')
+        self.ui.saveButton.clicked.connect(measure)
 
 
 if __name__ == '__main__':
