7c8cb97648cd17742a242aea9e7bda2e844afab6
RosSample <ros.3@mail.ru>
2021-10-27 10:17:38 +0300

    executable test

--- design/newmain.py
+++ design/newmain.py
@@ -16,63 +16,6 @@ def log_write(text):  # запись в лог
     return
 
 
-# def scan():  # сканирование штрих кода в течении 5 секунд
-#     time.sleep(1)
-#     ser.write("sc".encode())
-#     print("[" + greenwich_time + "] Сканирование штрих-кода")
-#     time.sleep(5)
-#     return
-
-
-# def calib():  # калибровка  status: Калибровка...
-#     calib_button_press = True  # плейсхолдер нажатия на кнопку в приложении
-#     time.sleep(1)
-#     ser.write("c".encode())
-#     if calib_button_press:  # имитация нажатия кнопки
-#         time.sleep(1)
-#         ser.write("c1".encode())
-#         for i in range(0, 4):
-#             time.sleep(1)
-#             if calib_button_press:  # нажатие кнопки еще раз
-#                 time.sleep(1)
-#                 ser.write("c2".encode())
-#             else:  # выход из калибровки при нажатии другой кнопки
-#                 ser.write("0".encode())
-#                 return
-#     else:  # выход из калибровки при нажатии другой кнопки
-#         ser.write("0".encode())
-#         return
-#     a = ser.readline().strip().decode()  # получаем 4 строки данных для записи в лог: calibration_coefficient_sample,
-#     b = ser.readline().strip().decode()  # calibration_coefficient_calib, w_calib[0] и w_sample[0]
-#     c = ser.readline().strip().decode()
-#     d = ser.readline().strip().decode()
-#     print("[" + greenwich_time + "] Калибровка прошла успешно")
-#     calib_settings = open("calib.txt", "w", encoding='utf8')  # открытие/создание настроек калибровки
-#     calib_settings.write(a + " " + b + " " + c + " " + d)  # сохранение настроек калибровки в файл
-#     calib_settings.close()
-#     log_write("[" + greenwich_time + "]" + " " + a + " " + b + " " + c + " " + d)  # запись настроек в лог
-#     time.sleep(1)
-#     return
-
-
-# def save():  # сохранение  status: Сохранение...
-#     if not card:
-#         print("Отсутствует карта")
-#         return
-#     save_button_press = True
-#     time.sleep(1)
-#     ser.write("s".encode())
-#     if save_button_press:  # имитация нажатия кнопки
-#         time.sleep(1)
-#         ser.write("s1".encode())
-#         print("Успешно сохранено")
-#     else:  # выход из сохранения при нажатии другой кнопки
-#         ser.write("0".encode())
-#         return
-#     time.sleep(1)
-#     return
-
-
 def port_search():  # поиск портов
     ports = ['COM%s' % (i + 1) for i in range(256)]
 
@@ -104,7 +47,7 @@ class MyWindow(QtWidgets.QMainWindow):
         self.show()
         self.ui.weighButton.clicked.connect(self.measure)
         self.ui.saveButton.clicked.connect(self.save)
-        # self.ui.scanButton.clicked.connect(self.scan)
+        self.ui.scanButton.clicked.connect(self.scan)
         self.ui.calibButton.clicked.connect(self.calib)
         self.ui.connectButton.clicked.connect(self.port_add)
         self.ui.disconnectButton.clicked.connect(self.port_disconnect)
@@ -221,7 +164,15 @@ class MyWindow(QtWidgets.QMainWindow):
         calib_settings.close()
         greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
         log_write("[" + greenwich_time + "] " + a + " " + b + " " + c + " " + d)  # запись настроек в лог
+        self.ui.label.setText("работает")
         time.sleep(1)
+        return
+
+    def scan(self):  # сканирование штрих кода в течении 5 секунд
+        greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+        self.add_text("[" + greenwich_time + "] Сканирование прошло успешно.")
+        ser.write("sc".encode())
+        time.sleep(5)
         return
 
     def port_connect(self):
