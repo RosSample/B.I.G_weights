c729933bbaacdcbb6dc51e26886a439b75135ea8
RosSample <ros.3@mail.ru>
2021-10-27 09:03:13 +0300

    pre-final?

--- design/newmain.py
+++ design/newmain.py
@@ -16,12 +16,12 @@ def log_write(text):  # запись в лог
     return
 
 
-def scan():  # сканирование штрих кода в течении 5 секунд
-    time.sleep(1)
-    ser.write("sc".encode())
-    print("[" + greenwich_time + "] Сканирование штрих-кода")
-    time.sleep(5)
-    return
+# def scan():  # сканирование штрих кода в течении 5 секунд
+#     time.sleep(1)
+#     ser.write("sc".encode())
+#     print("[" + greenwich_time + "] Сканирование штрих-кода")
+#     time.sleep(5)
+#     return
 
 
 # def calib():  # калибровка  status: Калибровка...
@@ -87,9 +87,9 @@ def port_search():  # поиск портов
     return result
 
 
-class mywindow(QtWidgets.QMainWindow):
+class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(mywindow, self).__init__()
+        super(MyWindow, self).__init__()
 
         self.weighButtonClickedCount = 0
         self.saveButtonClickedCount = 0
@@ -104,14 +104,14 @@ class mywindow(QtWidgets.QMainWindow):
         self.show()
         self.ui.weighButton.clicked.connect(self.measure)
         self.ui.saveButton.clicked.connect(self.save)
-        self.ui.scanButton.clicked.connect(scan)
+        # self.ui.scanButton.clicked.connect(self.scan)
         self.ui.calibButton.clicked.connect(self.calib)
         self.ui.connectButton.clicked.connect(self.port_add)
         self.ui.disconnectButton.clicked.connect(self.port_disconnect)
 
         self.ui.selectionWindow.activated.connect(self.port_connect)
 
-    def addText(self, text):
+    def add_text(self, text):
         self.ui.textShow.setText(self.ui.textShow.text() + text + "\n")
 
     def measure(self):
@@ -119,9 +119,9 @@ class mywindow(QtWidgets.QMainWindow):
             time.sleep(1)
             ser.write("m".encode())
             greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
-            self.addText("[" + greenwich_time + "] Измерение...")
+            self.add_text("[" + greenwich_time + "] Измерение...")
             self.ui.label.setText("Измерение")
-            self.addText("[" + greenwich_time + "] Поставьте груз и нажмите еще раз.")
+            self.add_text("[" + greenwich_time + "] Поставьте груз и нажмите взвесить.")
             self.weighButtonClickedCount += 1
             return
         self.weighButtonClickedCount = 0
@@ -134,7 +134,7 @@ class mywindow(QtWidgets.QMainWindow):
         greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
         log_write(
             "[" + counter + " " + sample_index + " " + greenwich_time + "] " + weight)
-        self.addText(
+        self.add_text(
             "[" + counter + " " + sample_index + " " + greenwich_time + "] Вес = " + weight + ".")
         self.ui.label.setText("работает")
         time.sleep(1)
@@ -143,16 +143,16 @@ class mywindow(QtWidgets.QMainWindow):
     def save(self):  # сохранение  status: Сохранение...
         if not card:
             greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
-            self.addText("[" + greenwich_time + "] Отсустствует карта памяти.")
+            self.add_text("[" + greenwich_time + "] Отсутствует карта памяти.")
             return
 
         if self.saveButtonClickedCount == 0:
             time.sleep(1)
             # ser.write("s".encode())
             greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
-            self.addText("[" + greenwich_time + "] Сохранение...")
+            self.add_text("[" + greenwich_time + "] Сохранение...")
             self.ui.label.setText("сохранение")
-            self.addText("[" + greenwich_time + "] Нажмите взвесить чтобы продолжить.")
+            self.add_text("[" + greenwich_time + "] Нажмите сохранить чтобы продолжить.")
             self.saveButtonClickedCount += 1
             return
         self.saveButtonClickedCount = 0
@@ -160,62 +160,83 @@ class mywindow(QtWidgets.QMainWindow):
         time.sleep(1)
         ser.write("s1".encode())
         greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
-        self.addText("[" + greenwich_time + "] Успешно сохранено.")
+        self.add_text("[" + greenwich_time + "] Успешно сохранено.")
         self.ui.label.setText("работает")
         time.sleep(1)
         return
 
     def calib(self):  # калибровка  status: Калибровка...
-        if self.weighButtonClickedCount == 0:
+        if self.calibButtonClickedCount == 0:
             time.sleep(1)
             ser.write("c".encode())
             greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
-            self.addText("[" + greenwich_time + "] Начать калибровку?")
+            self.add_text("[" + greenwich_time + "] Начать калибровку?")
             self.ui.label.setText("Измерение")
-            self.addText("[" + greenwich_time + "]" + " Чтобы продолжить нажмите калибровать")
-            self.addText("[" + greenwich_time + "]" + " Чтобы продолжить нажмите сохранить")
-            self.weighButtonClickedCount += 1
+            self.add_text("[" + greenwich_time + "]" + " Чтобы продолжить нажмите калибровать.")
+            self.add_text("[" + greenwich_time + "]" + " Для отмены нажмите сохранить.")
+            self.calibButtonClickedCount += 1
+            self.ui.label.setText("калибровка")
             return
 
-        time.sleep(1)
-        ser.write("c".encode())
-        if calib_button_press:  # имитация нажатия кнопки
+        if self.calibButtonClickedCount == 1:
             time.sleep(1)
             ser.write("c1".encode())
-            for i in range(0, 4):
-                time.sleep(1)
-                if calib_button_press:  # нажатие кнопки еще раз
-                    time.sleep(1)
-                    ser.write("c2".encode())
-                else:  # выход из калибровки при нажатии другой кнопки
-                    ser.write("0".encode())
-                    return
-        else:  # выход из калибровки при нажатии другой кнопки
-            ser.write("0".encode())
+            greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+            self.add_text("[" + greenwich_time + "]" + " Калибровка: 0.")
+            self.add_text("[" + greenwich_time + "]" + " Нажмите калибровать еще раз.")
+            self.calibButtonClickedCount += 1
             return
+
+        if self.calibButtonClickedCount == 2:
+            time.sleep(1)
+            ser.write("c2".encode())
+            greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+            self.add_text("[" + greenwich_time + "]" + " Калибровка: 20.")
+            self.add_text("[" + greenwich_time + "]" + " Нажмите калибровать еще раз.")
+            self.calibButtonClickedCount += 1
+            return
+
+        if self.calibButtonClickedCount == 3:
+            time.sleep(1)
+            ser.write("c2".encode())
+            greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+            self.add_text("[" + greenwich_time + "]" + " Калибровка: 40.")
+            self.add_text("[" + greenwich_time + "]" + " Нажмите калибровать еще раз.")
+            self.calibButtonClickedCount += 1
+            return
+
+        if self.calibButtonClickedCount == 4:
+            time.sleep(1)
+            ser.write("c2".encode())
+            greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+            self.add_text("[" + greenwich_time + "]" + " Калибровка прошла успешна")
+            self.calibButtonClickedCount = 0
+
         a = ser.readline().strip().decode()  # получаем 4 строки данных для записи в лог: calibration_coefficient_sample
         b = ser.readline().strip().decode()  # calibration_coefficient_calib, w_calib[0] и w_sample[0]
         c = ser.readline().strip().decode()
         d = ser.readline().strip().decode()
-        greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
-        self.addText("[" + greenwich_time + "] Калибровка прошла успешно")
         calib_settings = open("calib.txt", "w", encoding='utf8')  # открытие/создание настроек калибровки
         calib_settings.write(a + " " + b + " " + c + " " + d)  # сохранение настроек калибровки в файл
         calib_settings.close()
-        log_write("[" + greenwich_time + "]" + " " + a + " " + b + " " + c + " " + d)  # запись настроек в лог
+        greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+        log_write("[" + greenwich_time + "] " + a + " " + b + " " + c + " " + d)  # запись настроек в лог
         time.sleep(1)
         return
 
     def port_connect(self):
         port = self.sender().currentText()
-        self.addText(port)
+        greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+        self.add_text("[" + greenwich_time + "] Подключено к: " + port)
         global ser
         ser = serial.Serial(port)
 
         global card
-        self.addText(ser.readline().strip().decode())  # чтение первой строки из serial порта
+        greenwich_time = str(datetime.datetime.utcnow())[:19]  # время по гринвичу
+        self.add_text("[" + greenwich_time + "] " +
+                      ser.readline().strip().decode())  # чтение первой строки из serial порта
         cardln = ser.readline().strip().decode()  # чтение второй строки из serial порта
-        self.addText(cardln)
+        self.add_text("[" + greenwich_time + "] " + cardln)
         if cardln == "Card initialized.":  # проверка наличия sd карты
             card = True
         else:
@@ -232,7 +253,7 @@ class mywindow(QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    application = mywindow()
+    application = MyWindow()
     application.show()
 
     sys.exit(app.exec())
